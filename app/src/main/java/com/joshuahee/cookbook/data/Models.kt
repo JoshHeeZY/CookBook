2193e81e18191cea8fc9039aa2d88260784c4554
Joshua Zheng Yang Hee <joshuahee2001@gmail.com>
2025-09-26 15:05:39 +1000

    Add category grid layout

--- app/src/main/java/com/joshuahee/cookbook/data/Models.kt
+++ app/src/main/java/com/joshuahee/cookbook/data/Models.kt
@@ -12,3 +12,10 @@ data class Ingredient(
     val name: String,
     val amount: String
 )
+
+data class Category(
+    val id: String,
+    val name: String,
+    val imageUrl: String? = null,
+    val count: Int = 0
+)
